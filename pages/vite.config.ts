413b0b6bdfa738319d3cd29c748380ed80f89457
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-08-24 19:42:29 +0800

    chore(pages): ➕ add `@intlify/vite-plugin-vue-i18n vite-plugin-vue-markdown`

--- pages/vite.config.ts
+++ pages/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueI18n from '@intlify/vite-plugin-vue-i18n'
+import Markdown from 'vite-plugin-vue-markdown'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue({ include: [/\.vue$/, /\.md$/] }),
+    vueJsx(),
+    vueI18n({
+      include: resolve('src/locales/**'),
+    }),
+    Markdown(),
+  ],
 })
