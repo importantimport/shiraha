749de07b5983f3a902caa3fd83baa5bedef9cea0
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-12-09 14:50:03 +0800

    refactor(postcss): 🔥 remove mixins

--- packages/shiraha/postcss.config.js
+++ packages/shiraha/postcss.config.js
@@ -14,24 +14,7 @@ export default ({ env, file }) => ({
   parser: file.extname === '.sss' ? sugarss : false,
   plugins: [
     postcssEasyImport({ extensions: ['.sss', '.css'] }),
-    postcssMixins({
-      mixins: {
-        theme: (_mixin, colorScheme) => ({
-          '&': {
-            '--shiraha-color-background-h': `var(--shiraha-color-background-h-${colorScheme})`,
-            '--shiraha-color-background-s': `var(--shiraha-color-background-s-${colorScheme})`,
-            '--shiraha-color-background-l': `var(--shiraha-color-background-l-${colorScheme})`,
-            '--shiraha-color-content-h': `var(--shiraha-color-content-h-${colorScheme})`,
-            '--shiraha-color-content-s': `var(--shiraha-color-content-s-${colorScheme})`,
-            '--shiraha-color-content-l': `var(--shiraha-color-content-l-${colorScheme})`,
-            '--shiraha-color-primary-h': `var(--shiraha-color-primary-h-${colorScheme})`,
-            '--shiraha-color-primary-s': `var(--shiraha-color-primary-s-${colorScheme})`,
-            '--shiraha-color-primary-l': `var(--shiraha-color-primary-l-${colorScheme})`,
-            'color-scheme': colorScheme,
-          },
-        }),
-      },
-    }),
+    postcssMixins(),
     postcssJitProps(OpenProps),
     combineSelectors(),
     combineMediaQuery(),
