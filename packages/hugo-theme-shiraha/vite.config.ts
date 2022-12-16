83a625576863cab825143b8745ca94730b7c3b74
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-12-16 14:41:01 +0800

    chore: ➕ add `vite-hugo-plugin`

--- packages/hugo-theme-shiraha/vite.config.ts
+++ packages/hugo-theme-shiraha/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import hugoPlugin from 'vite-hugo-plugin'
+import { fileURLToPath } from 'node:url'
+import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    (hugoPlugin as any)({
+      appDir: fileURLToPath(import.meta.url),
+      hugoOutDir: resolve(fileURLToPath(import.meta.url), 'public'),
+    }),
+  ],
 })
