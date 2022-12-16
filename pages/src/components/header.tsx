cee4c01a7045b9a094d26a7b0f65e2c9ce5975d1
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-12-16 22:33:00 +0800

    refactor(pages/header): ♻️ show version

--- pages/src/components/header.tsx
+++ pages/src/components/header.tsx
@@ -1,12 +1,13 @@
 import { defineComponent } from 'vue'
+import { version } from '../../package.json'
 
 export default defineComponent({
   setup: () => () =>
     (
       <header>
         <h1>
-          <strong>❄ Shiraha</strong>
-          <sub>Classless CSS Framework</sub>
+          <span>❄ Shiraha</span>
+          <code>{version}</code>
         </h1>
         <nav>
           <a href="/">Home</a>
