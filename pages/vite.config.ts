d83d170c1c882c4277d2ff278b7e16ddeb21cda9
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-12-17 17:40:56 +0800

    chore: ➖ remove `vite-plugin-restart`

--- pages/vite.config.ts
+++ pages/vite.config.ts
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
 import Markdown from 'vite-plugin-vue-markdown'
-import ViteRestart from 'vite-plugin-restart'
 import eslint from 'vite-plugin-eslint'
 // postcss config
 import { vite as postcss } from '../postcss.config.js'
@@ -20,9 +19,6 @@ export default defineConfig({
       include: resolve('src/locales/**')
     }),
     Markdown(),
-    (ViteRestart instanceof Function ? ViteRestart : ViteRestart['default'])({
-      restart: ['../packages/shiraha/src/**']
-    }),
     eslint()
   ]
 })
