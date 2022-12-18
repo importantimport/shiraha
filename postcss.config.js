bedee7427d7d154c7e3c6f8f51342b2aeb3c25cb
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-12-18 21:23:17 +0800

    refactor: ⚡️ size optimization
    ➕ add postcss-custom-media, postcss-prune-var

--- postcss.config.js
+++ postcss.config.js
@@ -3,28 +3,29 @@ import sugarss from 'sugarss'
 // plugins
 import postcssImport from 'postcss-import'
 import postcssImportExtGlob from 'postcss-import-ext-glob'
+import postcssCustomMedia from 'postcss-custom-media'
 import postcssMixins from 'postcss-mixins'
 import postcssJitProps from 'postcss-jit-props'
-import postcssLightningCSS from 'postcss-lightningcss'
 import combineSelectors from 'postcss-combine-duplicated-selectors'
 import combineMediaQuery from 'postcss-combine-media-query'
+import pruneVar from 'postcss-prune-var'
+import postcssLightningCSS from 'postcss-lightningcss'
 // open-props
 import OpenProps from 'open-props'
 
 const plugins = (options) => [
   postcssImportExtGlob(),
   postcssImport(),
+  postcssCustomMedia(),
   postcssMixins(),
   postcssJitProps(OpenProps),
   combineSelectors(),
   combineMediaQuery(),
+  pruneVar(),
   postcssLightningCSS({
     browsers: 'defaults and supports css-cascade-layers',
     lightningcssOptions: {
-      drafts: {
-        nesting: true,
-        customMedia: true,
-      },
+      drafts: { nesting: true },
       ...options,
     },
   }),
