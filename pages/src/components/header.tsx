70ccb628cb72f5cf7165e5b0b4dc92507bf61a4e
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-12-18 15:54:47 +0800

    chore: 📝 update pages

--- pages/src/components/header.tsx
+++ pages/src/components/header.tsx
@@ -6,14 +6,17 @@ export default defineComponent({
     (
       <header>
         <h1>
-          <span>❄ Shiraha </span>
+          <span>❄Shiraha&nbsp;</span>
           <code>{version}</code>
         </h1>
+        <h5>A beautiful classless CSS framework with Sugar.</h5>
         <nav>
           <ul>
             <li>
               <a href="/">Home</a>
             </li>
+          </ul>
+          <ul>
             <li>
               <a href="https://github.com/importantimport/shiraha">GitHub</a>
             </li>
--- pages/src/components/intro.md
+++ pages/src/components/intro.md
@@ -1,5 +1,3 @@
-A beautiful classless CSS framework with Sugar.
-
 ```html
 <head>
   <link rel="stylesheet" href="https://unpkg.com/shiraha" />
