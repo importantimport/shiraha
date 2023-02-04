3e95c7340ddc36df12a1cc2897f4583275db2f87
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-02-04 15:38:26 +0800

    refactor(docs): ♻️ import theme from shiraha

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,6 @@
 import DefaultTheme from 'vitepress/theme'
 
-// import '../../../packages/shiraha-extra/src/vitepress/themes/yuki.sss'
+import '../../../packages/shiraha/src/themes/yuki.sss'
 import '../../../packages/shiraha-extra/src/vitepress/vars.sss'
 
 export default DefaultTheme
