623a6da7750cccadb9e4e12c7195cb9194525e31
kwaa <kwaa@users.noreply.github.com>
2023-04-27 13:13:36 +0000

    deploy: 3e8d2e58776d87d7a74d17d66f4816a7ce9ba64f

---
+++ assets/footer.stories-3b668eff.js
@@ -0,0 +1,118 @@
+import{x as p}from"./lit-element-2d314a70.js";const e={render:()=>p`<footer>
+    <img
+      src="https://relay.material.io/static/assets/relay_logo.svg"
+      alt="relay-logo"
+      height="64"
+      width="188"
+    />
+    <br />
+    <p>
+      Relay is changing the way designers and developers collaborate to build
+      UI, simplifying UI handoff by connecting design tools losslessly to code.
+    </p>
+    <br />
+    <p>
+      <a href="https://github.com/importantimport/shiraha">GitHub</a>
+      <span>&nbsp;&nbsp;</span>
+      <a href="https://github.com/importantimport/shiraha">GitLab</a>
+    </p>
+    <hr />
+    <span>© 2023 [DATA EXPUNGED] · Powered by Shiraha</span>
+  </footer>`},a={render:()=>p`<footer>
+    <hr />
+    <section>
+      <span>
+        Material Design is an adaptable system of guidelines, components, and
+        tools that support the best practices of user interface design. Backed
+        by open-source code, Material Design streamlines collaboration between
+        designers and developers, and helps teams quickly build beautiful
+        products.
+      </span>
+    </section>
+    <section>
+      <ul>
+        <li>
+          <span>Shiraha</span>
+        </li>
+        <li>
+          <a href="https://github.com/importantimport/shiraha">
+            Privacy Policy
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/importantimport/shiraha">
+            Terms of Service
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/importantimport/shiraha">
+            Join research studies
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/importantimport/shiraha">Feedback</a>
+        </li>
+      </ul>
+    </section>
+  </footer>`},l={title:"Footer"};var t,s,n;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: () => html\`<footer>
+    <img
+      src="https://relay.material.io/static/assets/relay_logo.svg"
+      alt="relay-logo"
+      height="64"
+      width="188"
+    />
+    <br />
+    <p>
+      Relay is changing the way designers and developers collaborate to build
+      UI, simplifying UI handoff by connecting design tools losslessly to code.
+    </p>
+    <br />
+    <p>
+      <a href="https://github.com/importantimport/shiraha">GitHub</a>
+      <span>&nbsp;&nbsp;</span>
+      <a href="https://github.com/importantimport/shiraha">GitLab</a>
+    </p>
+    <hr />
+    <span>© 2023 [DATA EXPUNGED] · Powered by Shiraha</span>
+  </footer>\`
+}`,...(n=(s=e.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};var i,r,o;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: () => html\`<footer>
+    <hr />
+    <section>
+      <span>
+        Material Design is an adaptable system of guidelines, components, and
+        tools that support the best practices of user interface design. Backed
+        by open-source code, Material Design streamlines collaboration between
+        designers and developers, and helps teams quickly build beautiful
+        products.
+      </span>
+    </section>
+    <section>
+      <ul>
+        <li>
+          <span>Shiraha</span>
+        </li>
+        <li>
+          <a href="https://github.com/importantimport/shiraha">
+            Privacy Policy
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/importantimport/shiraha">
+            Terms of Service
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/importantimport/shiraha">
+            Join research studies
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/importantimport/shiraha">Feedback</a>
+        </li>
+      </ul>
+    </section>
+  </footer>\`
+}`,...(o=(r=a.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const c=["Relay","M3"];export{a as M3,e as Relay,c as __namedExportsOrder,l as default};
+//# sourceMappingURL=footer.stories-3b668eff.js.map
---
+++ assets/preview-bd1a69fc.js
@@ -0,0 +1,2 @@
+const o={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},html:{root:"#root-inner",removeComments:!0}}};export{o as default};
+//# sourceMappingURL=preview-bd1a69fc.js.map
