61a0d2071ac2646487bbd48b1223f875ac785046
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-04-27 18:00:27 +0800

    refactor(docs/preview): 🔧 import shiraha

--- docs/.storybook/preview.ts
+++ docs/.storybook/preview.ts
@@ -1,5 +1,8 @@
 import type { Preview } from '@storybook/web-components'
 
+import 'shiraha/src/shiraha.sss'
+import 'shiraha/src/themes/yuki.sss'
+
 export default {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
