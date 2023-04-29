b320fca13d68eb96d957fd70437fd862940172a2
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-04-29 21:23:20 +0800

    refactor(header): ♻️ update styles & stories

--- docs/src/stories/components/header.stories.ts
+++ docs/src/stories/components/header.stories.ts
@@ -1,6 +1,16 @@
 import type { Meta, StoryObj } from '@storybook/web-components'
 import { html } from 'lit'
 
+export const SimpleHeader = {
+  render: () => html`
+    <header>
+      <nav>
+        <h2>Title Large</h2>
+      </nav>
+    </header>
+  `,
+}
+
 export const CenterAlignedHeader = {
   render: () => html`
     <header>
@@ -23,6 +33,36 @@ export const SmallHeader: StoryObj = {
         <a href="#" class="material-symbols-outlined">today</a>
         <a href="#" class="material-symbols-outlined">more_vert</a>
       </nav>
+    </header>
+  `,
+}
+
+export const MediumHeader: StoryObj = {
+  render: () => html`
+    <header>
+      <nav>
+        <a href="#" class="material-symbols-outlined">arrow_back</a>
+        <h2></h2>
+        <a href="#" class="material-symbols-outlined">attach_file</a>
+        <a href="#" class="material-symbols-outlined">today</a>
+        <a href="#" class="material-symbols-outlined">more_vert</a>
+      </nav>
+      <p>Headline Small</p>
+    </header>
+  `,
+}
+
+export const LargeHeader: StoryObj = {
+  render: () => html`
+    <header>
+      <nav>
+        <a href="#" class="material-symbols-outlined">arrow_back</a>
+        <h2></h2>
+        <a href="#" class="material-symbols-outlined">attach_file</a>
+        <a href="#" class="material-symbols-outlined">today</a>
+        <a href="#" class="material-symbols-outlined">more_vert</a>
+      </nav>
+      <h1>Headline Medium</h1>
     </header>
   `,
 }
