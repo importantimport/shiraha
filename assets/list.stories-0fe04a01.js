3ed287a205812437fefca3ed2d491546aea70a7a
Sevichecc <Sevichecc@users.noreply.github.com>
2023-04-30 18:29:51 +0000

    deploy: 920594780f9c3638902d547a3e2886dd088dac03

---
+++ assets/list.stories-0fe04a01.js
@@ -0,0 +1,42 @@
+import{x as n}from"./lit-element-2d314a70.js";const i={render:()=>n` <h3>Definition list</h3>
+    <dl>
+      <dt>Definition List Title</dt>
+      <dd>This is a definition list division.</dd>
+
+      <dt>Definition List Title 2</dt>
+      <dd>This is a definition list division item with a lot more to say.</dd>
+    </dl>
+    <h3>Ordered List</h3>
+    <ol>
+      <li>List Item 1</li>
+      <li>List Item 2</li>
+      <li>List Item 3</li>
+    </ol>
+    <h3>Unordered List</h3>
+    <ul>
+      <li>List Item 1</li>
+      <li>List Item 2</li>
+      <li>List Item 3</li>
+    </ul>`},d={title:"typography/List"};var t,s,e;i.parameters={...i.parameters,docs:{...(t=i.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: () => html\` <h3>Definition list</h3>
+    <dl>
+      <dt>Definition List Title</dt>
+      <dd>This is a definition list division.</dd>
+
+      <dt>Definition List Title 2</dt>
+      <dd>This is a definition list division item with a lot more to say.</dd>
+    </dl>
+    <h3>Ordered List</h3>
+    <ol>
+      <li>List Item 1</li>
+      <li>List Item 2</li>
+      <li>List Item 3</li>
+    </ol>
+    <h3>Unordered List</h3>
+    <ul>
+      <li>List Item 1</li>
+      <li>List Item 2</li>
+      <li>List Item 3</li>
+    </ul>\`
+}`,...(e=(s=i.parameters)==null?void 0:s.docs)==null?void 0:e.source}}};const o=["List"];export{i as List,o as __namedExportsOrder,d as default};
+//# sourceMappingURL=list.stories-0fe04a01.js.map
