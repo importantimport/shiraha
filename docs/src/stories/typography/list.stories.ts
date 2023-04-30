920594780f9c3638902d547a3e2886dd088dac03
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-05-01 02:28:05 +0800

    feat:✨Definition list style

--- docs/src/stories/typography/list.stories.ts
+++ docs/src/stories/typography/list.stories.ts
@@ -2,7 +2,15 @@ import type { Meta, StoryObj } from '@storybook/web-components'
 import { html } from 'lit'
 
 export const List: StoryObj = {
-  render: () => html`<h3>Ordered List</h3>
+  render: () => html` <h3>Definition list</h3>
+    <dl>
+      <dt>Definition List Title</dt>
+      <dd>This is a definition list division.</dd>
+
+      <dt>Definition List Title 2</dt>
+      <dd>This is a definition list division item with a lot more to say.</dd>
+    </dl>
+    <h3>Ordered List</h3>
     <ol>
       <li>List Item 1</li>
       <li>List Item 2</li>
