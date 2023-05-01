00c24f2107df149ceed549dccb1cd1eaa7f90135
kwaa <kwaa@users.noreply.github.com>
2023-05-01 08:51:39 +0000

    deploy: 53488f08ec42e71e8490410276365f17cc57641d

--- assets/list.stories-aeaee402.js
+++ assets/list.stories-aeaee402.js
@@ -1,4 +1,5 @@
-import{x as n}from"./lit-element-2d314a70.js";const i={render:()=>n` <h3>Definition list</h3>
+import{x as e}from"./lit-element-2d314a70.js";const i={render:()=>e`<main>
+    <h3>Definition list</h3>
     <dl>
       <dt>Definition List Title</dt>
       <dd>This is a definition list division.</dd>
@@ -17,8 +18,10 @@ import{x as n}from"./lit-element-2d314a70.js";const i={render:()=>n` <h3>Definit
       <li>List Item 1</li>
       <li>List Item 2</li>
       <li>List Item 3</li>
-    </ul>`},d={title:"typography/List"};var t,s,e;i.parameters={...i.parameters,docs:{...(t=i.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: () => html\` <h3>Definition list</h3>
+    </ul>
+  </main>`},d={title:"typography/List"};var t,n,s;i.parameters={...i.parameters,docs:{...(t=i.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: () => html\`<main>
+    <h3>Definition list</h3>
     <dl>
       <dt>Definition List Title</dt>
       <dd>This is a definition list division.</dd>
@@ -37,6 +40,7 @@ import{x as n}from"./lit-element-2d314a70.js";const i={render:()=>n` <h3>Definit
       <li>List Item 1</li>
       <li>List Item 2</li>
       <li>List Item 3</li>
-    </ul>\`
-}`,...(e=(s=i.parameters)==null?void 0:s.docs)==null?void 0:e.source}}};const o=["List"];export{i as List,o as __namedExportsOrder,d as default};
-//# sourceMappingURL=list.stories-0fe04a01.js.map
+    </ul>
+  </main>\`
+}`,...(s=(n=i.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const o=["List"];export{i as List,o as __namedExportsOrder,d as default};
+//# sourceMappingURL=list.stories-aeaee402.js.map
--- assets/preview-2051bd37.js
+++ assets/preview-2051bd37.js
@@ -1,2 +1,2 @@
 const o={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},options:{storySort:{order:["stories","components","typography","templates"]}},html:{root:"#root-inner",removeComments:!0}}};export{o as default};
-//# sourceMappingURL=preview-afb0536a.js.map
+//# sourceMappingURL=preview-2051bd37.js.map
