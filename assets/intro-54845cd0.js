d11ea06cfdcc558dc9efb6efe56f40a772c4aed1
kwaa <kwaa@users.noreply.github.com>
2023-05-01 15:10:54 +0000

    deploy: fd6c7883df9dfe5b9ea44d155fb26db9d8e1f534

---
+++ assets/intro-54845cd0.js
@@ -0,0 +1,17 @@
+import{j as s}from"./jsx-runtime-6eef64cc.js";import{u as i}from"./index-dc1d5b46.js";import"./index-c013ead5.js";import"./_commonjsHelpers-725317a4.js";function l(e={}){const{wrapper:a}=Object.assign({},i(),e.components);return a?s.jsx(a,Object.assign({},e,{children:s.jsx(n,{})})):n();function n(){const r=Object.assign({h1:"h1",h2:"h2",a:"a",p:"p",img:"img",pre:"pre",code:"code"},i(),e.components);return s.jsxs(s.Fragment,{children:[s.jsx(r.h1,{id:"-shiraha-docs",children:"❄ Shiraha Docs"}),`
+`,s.jsx("br",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx("br",{}),`
+`,s.jsx(r.h2,{id:"shiraha",children:s.jsx(r.a,{href:"/docs/stories-packages-shiraha--docs",children:"Shiraha"})}),`
+`,s.jsxs(r.p,{children:[s.jsx(r.a,{href:"https://npmjs.com/package/shiraha",target:"_blank",rel:"nofollow noopener noreferrer",children:s.jsx(r.img,{src:"https://img.shields.io/npm/v/shiraha",alt:"npm"})}),`
+`,s.jsx(r.img,{src:"https://img.badgesize.io/https:/unpkg.com/shiraha?compression=brotli",alt:"brotli size"})]}),`
+`,s.jsx(r.p,{children:"Material 3-inspired Classless CSS Framework."}),`
+`,s.jsx(r.pre,{children:s.jsx(r.code,{className:"language-html",children:`<link rel="stylesheet" href="https://unpkg.com/shiraha" />
+`})}),`
+`,s.jsx(r.h2,{id:"shiraha-colors",children:s.jsx(r.a,{href:"/docs/stories-packages-shiraha-colors--docs",children:"Shiraha Colors"})}),`
+`,s.jsxs(r.p,{children:[s.jsx(r.a,{href:"https://npmjs.com/package/shiraha-colors",target:"_blank",rel:"nofollow noopener noreferrer",children:s.jsx(r.img,{src:"https://img.shields.io/npm/v/shiraha-colors",alt:"npm"})}),`
+`,s.jsx(r.img,{src:"https://img.badgesize.io/https:/unpkg.com/shiraha-colors?compression=brotli",alt:"brotli size"})]}),`
+`,s.jsx(r.p,{children:"Browser script to generate colors for Shiraha / Material Web."}),`
+`,s.jsx(r.pre,{children:s.jsx(r.code,{className:"language-html",children:`<script type="module" src="https://esm.run/shiraha-colors" async><\/script>
+`})})]})}}export{l as default};
+//# sourceMappingURL=intro-54845cd0.js.map
--- assets/intro-62f2fbf0.js
+++
@@ -1,9 +0,0 @@
-import{j as s}from"./jsx-runtime-6eef64cc.js";import{u as o}from"./index-dc1d5b46.js";import"./index-c013ead5.js";import"./_commonjsHelpers-725317a4.js";function d(a={}){const{wrapper:e}=Object.assign({},o(),a.components);return e?s.jsx(e,Object.assign({},a,{children:s.jsx(n,{})})):n();function n(){const r=Object.assign({h1:"h1",h2:"h2",a:"a",p:"p"},o(),a.components);return s.jsxs(s.Fragment,{children:[s.jsx(r.h1,{id:"-shiraha-docs",children:"❄ Shiraha Docs"}),`
-`,s.jsx("br",{}),`
-`,s.jsx("br",{}),`
-`,s.jsx("br",{}),`
-`,s.jsx(r.h2,{id:"shiraha",children:s.jsx(r.a,{href:"/docs/stories-packages-shiraha--docs",children:"Shiraha"})}),`
-`,s.jsx(r.p,{children:"Material 3-inspired Classless CSS Framework."}),`
-`,s.jsx(r.h2,{id:"shiraha-colors",children:s.jsx(r.a,{href:"/docs/stories-packages-shiraha-colors--docs",children:"Shiraha Colors"})}),`
-`,s.jsx(r.p,{children:"Browser script to generate colors for Shiraha / Material Web."})]})}}export{d as default};
-//# sourceMappingURL=intro-62f2fbf0.js.map
