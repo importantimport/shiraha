68f765fd9561436a77a740f1f68cbcfd398de85d
kwaa <kwaa@users.noreply.github.com>
2023-05-01 10:10:04 +0000

    deploy: 2200b39f1d80e8b4695fa0de0bd3bfbd981f84f1

---
+++ assets/breadcrumb.stories-bbfedf07.js
@@ -0,0 +1,62 @@
+import{x as t}from"./lit-element-2d314a70.js";const a={render:()=>t`<nav>
+    <ol>
+      <li>
+        <a href="#">Shiraha</a>
+      </li>
+      <span>/</span>
+      <li>
+        <a href="#">Components</a>
+      </li>
+      <span>/</span>
+      <li>
+        <span>Breadcrumb</span>
+      </li>
+    </ol>
+  </nav>`},n={render:()=>t`<nav>
+    <ol>
+      <li>
+        <a href="#">Shiraha</a>
+      </li>
+      <span class="material-symbols-outlined">chevron_right</span>
+      <li>
+        <a href="#">Components</a>
+      </li>
+      <span class="material-symbols-outlined">chevron_right</span>
+      <li>
+        <span>Breadcrumb</span>
+      </li>
+    </ol>
+  </nav>`},c={title:"components/extra/Breadcrumb"};var r,e,s;a.parameters={...a.parameters,docs:{...(r=a.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: () => html\`<nav>
+    <ol>
+      <li>
+        <a href="#">Shiraha</a>
+      </li>
+      <span>/</span>
+      <li>
+        <a href="#">Components</a>
+      </li>
+      <span>/</span>
+      <li>
+        <span>Breadcrumb</span>
+      </li>
+    </ol>
+  </nav>\`
+}`,...(s=(e=a.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};var o,l,i;n.parameters={...n.parameters,docs:{...(o=n.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => html\`<nav>
+    <ol>
+      <li>
+        <a href="#">Shiraha</a>
+      </li>
+      <span class="material-symbols-outlined">chevron_right</span>
+      <li>
+        <a href="#">Components</a>
+      </li>
+      <span class="material-symbols-outlined">chevron_right</span>
+      <li>
+        <span>Breadcrumb</span>
+      </li>
+    </ol>
+  </nav>\`
+}`,...(i=(l=n.parameters)==null?void 0:l.docs)==null?void 0:i.source}}};const m=["Breadcrumb","BreadcrumbWithIcon"];export{a as Breadcrumb,n as BreadcrumbWithIcon,m as __namedExportsOrder,c as default};
+//# sourceMappingURL=breadcrumb.stories-bbfedf07.js.map
--- assets/preview-02d7a357.js
+++ assets/preview-02d7a357.js
@@ -1,2 +1,2 @@
 const o={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},options:{storySort:{order:["stories","components","typography","templates"]}},html:{root:"#root-inner",removeComments:!0}}};export{o as default};
-//# sourceMappingURL=preview-6ff1bb91.js.map
+//# sourceMappingURL=preview-02d7a357.js.map
