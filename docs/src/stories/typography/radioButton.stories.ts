f6721accac4e87d80868e7693c65b75a133505d8
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-05-02 22:17:49 +0800

    feat: ✨ Radio Button style

---
+++ docs/src/stories/typography/radioButton.stories.ts
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/web-components'
+import { html } from 'lit'
+
+export const RadioButton: StoryObj = {
+  render: () => html` <fieldset>
+    <legend>Radio buttons</legend>
+    <ul>
+      <li>
+        <label for="radio1"
+          ><input
+            id="radio1"
+            name="radio"
+            type="radio"
+            class="radio"
+            checked="checked"
+          />
+          checked</label
+        >
+      </li>
+      <li>
+        <label for="radio2"
+          ><input
+            id="radio2"
+            name="radio"
+            type="radio"
+            class="radio"
+          />uncheck</label
+        >
+      </li>
+      <li>
+        <label for="radio3"
+          ><input
+            id="radio3"
+            name="radio"
+            type="radio"
+            class="radio"
+            checked="checked"
+            disabled
+          />disabled</label
+        >
+      </li>
+    </ul>
+  </fieldset>`,
+}
+
+export default {
+  title: 'typography/RadioButton',
+} as Meta
