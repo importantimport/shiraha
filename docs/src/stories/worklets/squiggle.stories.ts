8b0731cb993884a9aa0ad67991c8d3541b14f6d1
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-03 21:33:45 +0800

    docs: 📝 add squiggle stories

---
+++ docs/src/stories/worklets/squiggle.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/web-components'
+import { html } from 'lit'
+
+import 'css-paint-polyfill'
+import 'shiraha-worklets'
+
+export const Squiggle: StoryObj = {
+  render: () => html`<style>
+      hr {
+        border: 0;
+        height: 8px;
+        background: paint(shiraha-squiggle);
+      }
+      main > hr:nth-child(3) {
+        height: 16px;
+      }
+      main > hr:last-child {
+        height: 32px;
+      }
+    </style>
+    <main>
+      <hr />
+      <br />
+      <hr />
+      <br />
+      <hr />
+    </main>`,
+}
+
+export default {
+  title: 'worklets/Squiggle',
+} as Meta
