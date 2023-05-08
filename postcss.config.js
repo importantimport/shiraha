533afda428645307ed8514fe18f8172a8a593ad8
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-08 16:45:42 +0800

    chore(postcss): 🔧 update config

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,18 @@
 // parser
 import sugarss from 'sugarss'
+
 // plugins
 import postcssImport from 'postcss-import'
 import postcssImportExtGlob from 'postcss-import-ext-glob'
 import postcssCustomMedia from 'postcss-custom-media'
 import postcssMixins from 'postcss-mixins'
 import postcssJitProps from 'postcss-jit-props'
+
 // import combineSelectors from 'postcss-combine-duplicated-selectors'
 import combineMediaQuery from 'postcss-combine-media-query'
 import pruneVar from 'postcss-prune-var'
 import postcssLightningCSS from 'postcss-lightningcss'
+
 // open-props
 import OpenProps from 'open-props'
 
@@ -55,7 +58,7 @@ export default ({ env, file }) => ({
       browsers: 'defaults and supports css-cascade-layers',
       lightningcssOptions: {
         drafts: { nesting: true },
-        minify: env === 'production' ? true : false,
+        minify: env === 'production',
       },
     }),
   ],
