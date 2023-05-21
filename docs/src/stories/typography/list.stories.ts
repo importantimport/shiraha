686ba1e34db0072bff8c1eea9e957fe06d4d3c8c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-21 18:27:21 +0800

    docs(typography/list): 📝 fix spaces

--- docs/src/stories/typography/list.stories.ts
+++ docs/src/stories/typography/list.stories.ts
@@ -29,8 +29,7 @@ export const List: StoryObj = {
         <li>List Item 1</li>
         <li>List Item 2</li>
         <li>List Item 3</li>
-        <li>
-          List Item 4
+        <li>List Item 4
           <ul>
             <li>List Item 5</li>
           </ul>
