a9208226489df3251e0722f6f4ae76d7ab1c2be3
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-29 14:47:39 +0800

    docs: 📝 add heading stories

---
+++ docs/src/stories/typography/headings.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/web-components'
+import { html } from 'lit'
+
+export const Headings: StoryObj = {
+  render: () => html`
+    <main>
+      <h1>Heading 1</h1>
+      <h2><a href="#">Heading 2 with Anchor</a></h2>
+      <h2><a href="https://example.com">Heading 2 with Link</a></h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <h5>Heading 5</h5>
+      <h6>Heading 6</h6>
+    </main>
+  `,
+}
+
+export default {
+  title: 'typography/Headings',
+} as Meta
