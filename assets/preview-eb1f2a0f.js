afd1fdf1d81067ff8db97ed248eee16fdd84de90
kwaa <kwaa@users.noreply.github.com>
2023-05-29 12:05:28 +0000

    deploy: d21b75ce289bf7c1d20dfc47e84799f434c4b387

--- assets/preview-eb1f2a0f.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-31da73dd.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-9069c4dc.js"),["./DocsRenderer-EYKKDMVH-9069c4dc.js","./iframe-31da73dd.js","./index-c013ead5.js","./_commonjsHelpers-725317a4.js","./index-4f05e47a.js","./chunk-4NMOSTKD-052c8761.js","./index-d475d2ea.js","./index-d37d4223.js","./index-d38538b0.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-eb1f2a0f.js.map
