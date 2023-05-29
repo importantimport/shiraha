413d18022f4e8082751b9e98dd29e659df17b776
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-29 15:01:04 +0800

    fix(headings): 🐛 support relative link

--- docs/src/stories/typography/headings.stories.ts
+++ docs/src/stories/typography/headings.stories.ts
@@ -6,7 +6,8 @@ export const Headings: StoryObj = {
     <main>
       <h1>Heading 1</h1>
       <h2><a href="#">Heading 2 with Anchor</a></h2>
-      <h2><a href="https://example.com">Heading 2 with Link</a></h2>
+      <h2><a href="/test">Heading 2 with Relative Link</a></h2>
+      <h2><a href="https://example.com">Heading 2 with Absolute Link</a></h2>
       <h3>Heading 3</h3>
       <h4>Heading 4</h4>
       <h5>Heading 5</h5>
