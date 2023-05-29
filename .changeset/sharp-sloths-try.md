21493d8cad81d2e08051e34d9f3a0de06b083b11
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-29 20:07:44 +0800

    fix(shiraha-colors): 🐛 fix selector rule

---
+++ .changeset/sharp-sloths-try.md
@@ -0,0 +1,5 @@
+---
+"shiraha-colors": patch
+---
+
+Fix: selector rule
--- packages/shiraha-colors/src/index.ts
+++ packages/shiraha-colors/src/index.ts
@@ -25,8 +25,8 @@ mutationObserver.observe(document.querySelector('title') as Node, {
 // eslint-disable-next-line unicorn/prefer-top-level-await
 (async () =>
   await applyShirahaColors(
-    document.querySelector(':not(.h-card) img:is(.u-featured, .u-photo)')
-    ?? document.querySelector(':not(.h-card) img[itemprop="image"]')
-    ?? document.querySelector(':not(.h-card) img'),
+    document.querySelector(':not(.h-card) > img:is(.u-featured, .u-photo)')
+    ?? document.querySelector(':not(.h-card) > img[itemprop="image"]')
+    ?? document.querySelector(':not(.h-card) > img'),
     window.shiraha?.colors,
   ))()
