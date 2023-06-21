786d034e6f6b8fe5d95caf9b039dab03058700a2
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-06-21 17:58:01 +0800

    refactor(eslint): 🔧 set project

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,13 @@
-export { default } from '@importantimport/eslint-config'
+import ii from '@importantimport/eslint-config'
+
+export default [
+  ...ii,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.json', './packages/*/tsconfig.json'],
+      },
+    },
+  },
+]
