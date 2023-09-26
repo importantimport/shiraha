77a71a5d66cf39e16b95b293f7b393ec3610dabc
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-26 23:54:46 +0800

    refactor(shiraha-ve): 💄 update styles

--- packages/shiraha-ve/src/base/all.global.css.ts
+++ packages/shiraha-ve/src/base/all.global.css.ts
@@ -3,8 +3,15 @@ import { globalStyle } from '@vanilla-extract/css'
 import { vars } from '../vars.css'
 
 globalStyle('*, ::before, ::after', {
-  borderColor: vars.color.primary,
-  borderStyle: 'solid',
-  borderWidth: 0,
-  boxSizing: 'border-box',
+  '@media': {
+    '(prefers-reduced-motion: reduce)': {
+      animationDuration: '0.01ms !important',
+      animationIterationCount: '1 !important',
+      transitionDuration: '0.01ms !important',
+    },
+  },
+  'borderColor': vars.color.primary,
+  'borderStyle': 'solid',
+  'borderWidth': 0,
+  'boxSizing': 'border-box',
 })
--- packages/shiraha-ve/src/base/body.global.css.ts
+++ packages/shiraha-ve/src/base/body.global.css.ts
@@ -7,6 +7,7 @@ globalStyle('body', {
   color: vars.color.onSurface,
   /** {@link https://github.com/oscarotero/css-style-guide/tree/master/cases/notch} */
   padding: 'env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left)',
+  textRendering: 'optimizeSpeed',
 })
 
 /** {@link https://github.com/oscarotero/semantic-html/blob/master/html/page.md#main-element} */
