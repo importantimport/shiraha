f7e085c98ca1b08b0fbcb184b0b48dc6e15c75d7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-26 16:23:43 +0800

    refactor(shiraha-ve): 💄 update styles

--- docs-ve/histoire.setup.ts
+++ docs-ve/histoire.setup.ts
@@ -1,4 +1,10 @@
+// material-symbols
 import 'material-symbols/outlined.css'
+// shiraha-ve base
+import 'shiraha-ve/base/all.global.css'
+import 'shiraha-ve/base/body.global.css'
+import 'shiraha-ve/base/html.global.css'
+import 'shiraha-ve/base/main.global.css'
 import 'shiraha-ve/base/reset.css'
 
 // import 'shiraha-ve/themes/baseline.css'
--- packages/shiraha-ve/src/base/all.global.css.ts
+++ packages/shiraha-ve/src/base/all.global.css.ts
@@ -1,5 +1,10 @@
 import { globalStyle } from '@vanilla-extract/css'
 
+import { vars } from '../vars.css'
+
 globalStyle('*, ::before, ::after', {
+  borderColor: vars.color.primary,
+  borderStyle: 'solid',
+  borderWidth: 0,
   boxSizing: 'border-box',
 })
