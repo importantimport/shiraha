93aad53eed28f42684e420174ee5f14be865def2
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-27 13:06:10 +0800

    refactor(shiraha-ve): 💄 update styles

--- packages/shiraha-ve/src/components/tabs/vanilla.css.ts
+++ packages/shiraha-ve/src/components/tabs/vanilla.css.ts
@@ -1,25 +1,34 @@
 import { style } from '@vanilla-extract/css'
 
+import { opacity } from '../../utils/color'
 import { vars } from '../../vars.css'
 
 /** `[role="tablist"]` */
 export const tablist = style({
+  borderBottomColor: vars.color.surfaceVariant,
+  borderBottomWidth: 1,
   display: 'flex',
   width: '100%',
 })
 
 /** `[role="tab"]` */
 export const tab = style({
-  alignItems: 'center',
-  borderBottomColor: vars.color.surfaceVariant,
-  borderBottomWidth: 1,
-  display: 'inline-flex',
-  height: 48,
-  justifyContent: 'center',
-  width: '100%',
+  ':focus-visible': {
+    backgroundColor: opacity(vars.color.onSurface, 12),
+    outline: `${vars.color.primary} auto 2px`,
+  },
+  ':hover': {
+    backgroundColor: opacity(vars.color.onSurface, 8),
+  },
+  'alignItems': 'center',
+  'display': 'inline-flex',
+  'height': 48,
+  'justifyContent': 'center',
+  'width': '100%',
 })
 
 export const tabActive = style({
+  backgroundColor: opacity(vars.color.onSurface, 12),
   borderBottomColor: vars.color.primary,
   borderBottomWidth: 2,
 })
