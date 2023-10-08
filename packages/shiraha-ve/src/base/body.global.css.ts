b763dd1620cf05e2cbf2b250cf277cb01c616991
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-10-08 22:33:15 +0800

    refactor(shiraha-ve): 💄 update styles

--- packages/shiraha-ve/src/base/body.global.css.ts
+++ packages/shiraha-ve/src/base/body.global.css.ts
@@ -1,13 +1,26 @@
 import { globalStyle } from '@vanilla-extract/css'
+import OP from 'open-props'
 
 import { vars } from '../vars.css'
 
 globalStyle('body', {
-  backgroundColor: vars.color.surface,
-  color: vars.color.onSurface,
+  /** {@link https://web.dev/adapting-typography-to-user-preferences-with-css} */
+  '@media': {
+    '(prefers-color-scheme: dark)': {
+      fontVariationSettings: '"GRAD" -25',
+    },
+    '(prefers-contrast: less)': {
+      fontWeight: OP.fontWeight2,
+    },
+    '(prefers-contrast: more)': {
+      fontWeight: OP.fontWeight6,
+    },
+  },
+  'backgroundColor': vars.color.surface,
+  'color': vars.color.onSurface,
   /** {@link https://github.com/oscarotero/css-style-guide/tree/master/cases/notch} */
-  padding: 'env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left)',
-  textRendering: 'optimizeSpeed',
+  'padding': 'env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left)',
+  'textRendering': 'optimizeSpeed',
 })
 
 /** {@link https://github.com/oscarotero/semantic-html/blob/master/html/page.md#main-element} */
--- packages/shiraha-ve/src/base/main.global.css.ts
+++ packages/shiraha-ve/src/base/main.global.css.ts
@@ -3,6 +3,7 @@ import OP from 'open-props'
 
 globalStyle('main', {
   boxSizing: 'content-box',
+  // TODO: maybe `createContainer`
   container: 'main / inline-size',
   maxWidth: OP.sizeMd, // 768px
   width: '100vh',
