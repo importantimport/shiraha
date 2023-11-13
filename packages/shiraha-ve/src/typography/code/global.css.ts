cdcc243a01070abd0e74a1831ac9f81b6021dd98
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-11-13 21:25:05 +0800

    refactor(shiraha-ve): 💄 code style

--- packages/shiraha-ve/src/typography/code/global.css.ts
+++ packages/shiraha-ve/src/typography/code/global.css.ts
@@ -3,14 +3,38 @@ import OP from 'open-props'
 
 import { vars } from '../../vars.css'
 
-globalStyle('code', {
+globalStyle(':where(code)', {
   backgroundColor: vars.color.surfaceVariant,
   borderRadius: 4,
   color: vars.color.onSurfaceVariant,
   fontFamily: OP.fontMono,
   fontSize: '0.875em',
+  fontWeight: OP.fontWeight6,
 })
 
-// globalStyle('pre', {
-//   overflowX: 'auto',
+globalStyle(':where(pre)', {
+  // TODO: adjust
+  borderRadius: '0.25rem',
+  overflowX: 'auto',
+  padding: '1.5rem',
+})
+
+globalStyle(':where(pre, code)', {
+  // background: 'transparent',
+  hyphens: 'none',
+  // tabSize: 4,
+  whiteSpace: 'pre',
+  wordBreak: 'normal',
+  wordSpacing: 'normal',
+  wordWrap: 'normal',
+})
+
+// globalStyle('a code', {
+//   fontWeight: vars.color.primary,
 // })
+
+globalStyle('pre code', {
+  fontWeight: 'inherit',
+})
+
+// globalStyle(':where(kbd)', {})
