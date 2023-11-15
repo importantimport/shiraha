f795b04b096714114f8e2f05e4fb13d220305951
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-11-15 20:26:54 +0800

    refactor(button): ♻️ update selectors

--- packages/shiraha-ve/src/components/button/global.css.ts
+++ packages/shiraha-ve/src/components/button/global.css.ts
@@ -11,36 +11,44 @@ const commonButton = ':where(button:not([role]), [role="button"], input[type="bu
 const submitButton = ':where(input[type="submit"])'
 const resetButton = ':where(input[type="reset"])'
 
+const disabled = ':disabled, [aria-disabled="true"]'
+
 // Global Button Style
 globalStyle(button, styles.baseButtonGlobal)
-globalStyle(`${button}:disabled`, styles.baseButtonDisabled)
+globalStyle(`${button}:is(${disabled})`, styles.baseButtonDisabled)
 globalStyle(`${button}:focus-visible`, styles.baseButtonFocusVisible)
-globalStyle(`${button}:active:not(:disabled)`, styles.baseButtonActive)
+globalStyle(`${button}:active:not(${disabled})`, styles.baseButtonActive)
 globalStyle(`${button}:has([class^="material-symbols"]:first-child:not(:last-child))`, styles.baseButtonWithIconChild)
 
 // Common Button (Elevated Style)
-globalStyle(commonButton, styles.elevatedButtonGlobal)
-globalStyle(`${commonButton}:active:not(:disabled)`, styles.elevatedButtonActive)
-globalStyle(`${commonButton}:focus:not(:disabled)`, styles.elevatedButtonFocus)
-globalStyle(`${commonButton}:hover:not(:active, :disabled)`, styles.elevatedButtonHover)
+globalStyle(`${commonButton}:not(${disabled})`, styles.elevatedButtonGlobal)
+globalStyle(`${commonButton}:active:not(${disabled})`, styles.elevatedButtonActive)
+globalStyle(`${commonButton}:focus:not(${disabled})`, styles.elevatedButtonFocus)
+globalStyle(`${commonButton}:hover:not(:active, ${disabled})`, styles.elevatedButtonHover)
 
 // Submit Button (Filled Style)
-globalStyle(submitButton, styles.filledButtonGlobal)
-globalStyle(`${submitButton}:active:not(:disabled)`, styles.filledButtonActive)
-globalStyle(`${submitButton}:focus:not(:disabled)`, styles.filledButtonFocus)
-globalStyle(`${submitButton}:hover:not(:active, :disabled)`, styles.filledButtonHover)
+globalStyle(`${submitButton}:not(${disabled})`, styles.filledButtonGlobal)
+globalStyle(`${submitButton}:active:not(${disabled})`, styles.filledButtonActive)
+globalStyle(`${submitButton}:focus:not(${disabled})`, styles.filledButtonFocus)
+globalStyle(`${submitButton}:hover:not(:active, ${disabled})`, styles.filledButtonHover)
 
 // Reset Button (Filled Style, Error Color)
-globalStyle(resetButton, {
+globalStyle(`${resetButton}:not(${disabled})`, {
   backgroundColor: vars.color.error,
   color: vars.color.onError,
 })
 
-globalStyle(`${resetButton}:is(:active, :focus):not(:disabled)`, {
+globalStyle(`${resetButton}:is(:active, :focus):not(${disabled})`, {
   backgroundColor: state(vars.color.onError, 12, vars.color.error),
 })
 
-globalStyle(`${resetButton}:hover:not(:disabled)`, {
+globalStyle(`${resetButton}:hover:not(${disabled})`, {
   backgroundColor: state(vars.color.onError, 8, vars.color.error),
   boxShadow: OP.shadow1,
 })
+
+/**
+ * Icon Button
+ * {@link https://github.com/oscarotero/semantic-html/blob/master/html/icon-link.md#icon-only-button}
+ */
+globalStyle(`${button}:is([aria-label])`, styles.iconButton)
