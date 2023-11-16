63f7d6c98d5e7e61230706c346c0d6e4ba24ec5f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-11-16 12:40:07 +0800

    chore: 🔧 update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,17 @@
 import { createFullConfig } from '@importantimport/eslint-config'
+import { GLOB_TS } from '@importantimport/eslint-config/utils/glob'
+import { createConfig } from '@importantimport/eslint-config-vanilla-extract'
 
 export default await createFullConfig({
   gitignore: false,
   ts: true,
   vanillaExtract: true,
+}, {
+  ...createConfig()[0],
+  // check styles.ts, styles.*.ts
+  files: [GLOB_TS],
+}, {
+  rules: {
+    'n/no-extraneous-import': 'off',
+  },
 })
